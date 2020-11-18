9de6af7807aa98cab60bed007bd818143bdc581c
Abdul Basit <chabdulbasit1122@gmail.com>
2020-11-19 02:35:04 +0500

    Creating React Boilerplate

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
+import { Navbar } from "./Components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <h1>Hello Guys. Welcome to Service Hub</h1>
       <h2>Developed by your very own, Ch Abdul Basit...</h2>
     </div>
