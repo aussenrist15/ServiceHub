feec26ec515334b065e63c5de797b505bcbfbb01
Abdul Basit <chabdulbasit1122@gmail.com>
2020-11-19 02:09:33 +0500

    Creating react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Guys. Welcome to Service Hub</h1>
+      <h2>Developed by your very own, Ch Abdul Basit...</h2>
+    </div>
+  );
+}
+
+export default App;
